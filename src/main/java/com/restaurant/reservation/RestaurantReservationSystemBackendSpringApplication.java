3a0f324f397da8c9a2a20696a08677c946e984da
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-03-17 20:35:16 +0200

    Initial commit: Spring Boot backend project

---
+++ src/main/java/com/restaurant/reservation/RestaurantReservationSystemBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationSystemBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationSystemBackendSpringApplication.class, args);
+	}
+
+}
