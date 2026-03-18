3c724eadb417461f7434845c93f47f0f0b33d351
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-03-18 20:13:20 +0200

    feat: add UserRequestDTO

---
+++ src/main/java/com/restaurant/reservation/user/dto/UserRequestDTO.java
@@ -0,0 +1,9 @@
+package com.restaurant.reservation.user.dto;
+
+public class UserRequestDTO {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+}
