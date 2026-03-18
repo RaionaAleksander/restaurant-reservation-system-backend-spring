c26f78f50248271a826a30501c9c94a23ce0f0ee
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-03-18 22:44:30 +0200

    feat: make email, password, firstName and lastName mandatory in UserRequestDTO

--- src/main/java/com/restaurant/reservation/user/dto/UserRequestDTO.java
+++ src/main/java/com/restaurant/reservation/user/dto/UserRequestDTO.java
@@ -1,9 +1,22 @@
 package com.restaurant.reservation.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
     private String phone;
 }
